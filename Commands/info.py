f15d1ec07c7d61913d288122cf527aebbb06fa55
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-05-01 15:53:16 -0500

    Update info.py

--- Commands/info.py
+++ Commands/info.py
@@ -54,8 +54,9 @@ class info(commands.Cog):
 
     @commands.command(aliases=["help"])
     async def hlep(self,ctx):
-        await ctx.author.send(embed=embedify('''```md
-#] !] PRIZ AI ;] [! COMMANDS LIST``````md
+        result = 0
+        def check(reaction, user): return user == ctx.author
+        lit = ["""
 ] ";]hlep"
 > Brings up this message :)
 ] ";]sys"
@@ -65,8 +66,8 @@ class info(commands.Cog):
 ] ";]slots"
 > Slot machine :D
 ] ";]coin {x}" [Option: {x}]
-> Flips a virtual coin {x} times 0.0
-] ";]git"
+> Flips a virtual coin {x} times 0.0""",
+                """] ";]git"
 > Shows the github/gitlab repo ;]
 ] ";]rng {x} {y} {z}" [Option: {z}]
 > Prints an RNG from {x} to {y}, {z} times
@@ -75,8 +76,8 @@ class info(commands.Cog):
 ] ";]dnd"
 > Roles all dice from Dragons and Dungeons!
 ] ";]info"
-> Shows additional info
-] ";]cool {uID}"
+> Shows additional info""",
+                """] ";]cool {uID}"
 > Gives someone [{userID}] a sneaky surpise ;D
 ] ";]rick"
 > Rick Roll! °ω°
@@ -85,8 +86,8 @@ class info(commands.Cog):
 ] ";]spam {x}"
 > Spams {x} amount of chars >.<
 ] ";]graph {eq} {xmin} {xmax}"
-> Graphs {eq} from {xmin} to {xmax} 9.6
-] ";]rto {x} {y}"
+> Graphs {eq} from {xmin} to {xmax} 9.6""",
+                """] ";]rto {x} {y}"
 > Reduces the ratio of {x} and {y} ;-;
 ] ";]rad {x}"
 > Reduces a radical, {x}! >:D
@@ -95,12 +96,55 @@ class info(commands.Cog):
 ] ";]stats {data}"
 > Gives stats given {data} ._.
 ] ";]quad {a} {b} {c}"
-> Uses {a}, {b}, and {c} to solve the Quad Formula 0o0`````md
+> Uses {a}, {b}, and {c} to solve the Quad Formula 0o0"""]
+    msg = await ctx.send(embed=embedify(f'''```md
+#] !] PRIZ AI ;] [! COMMANDS LIST``````md
+{lit[result]}`````md
 #] To see mod commands, use ";]hlepmod"
 #] To have a conversation, use "]{your text here}""
 #] Some of your data is stored, use ";]data" to see more
 ```'''))
-        await com("HELP")
+        await msg.add_reaction('⏪')
+        await msg.add_reaction('◀')
+        await msg.add_reaction('⏹')
+        await msg.add_reaction('▶')
+        await msg.add_reaction('⏩')
+        while True:
+            try:
+                reaction, user = await bot.wait_for('reaction_add', timeout=60.0, check=check)
+            except asyncio.TimeoutError: return await msg.clear_reactions()
+            else:
+                if str(reaction.emoji) == '⏪':
+                    result = 0
+                    await msg.remove_reaction('⏪', ctx.author)
+                    await msg.edit(embed=embedify(f'''```md
+#] !] PRIZ AI ;] [! COMMANDS LIST``````md
+{lit[result]}`````md
+#] To see mod commands, use ";]hlepmod"
+#] To have a conversation, use "]{your text here}"
+#] Some of your data is stored, use ";]data" to see more
+```'''))
+                elif str(reaction.emoji) == '◀':
+                    await msg.remove_reaction('◀', ctx.author)
+                    if result == 0: return
+                    else: result = result - 1
+                elif str(reaction.emoji) == '⏹':
+                    return await msg.clear_reactions()
+                elif str(reaction.emoji) == '▶':
+                    await msg.remove_reaction('▶', ctx.author)
+                    if result == (len(m.pages) - 1): return
+                    else: result += 1
+                elif str(reaction.emoji) == '⏩':
+                    result = len(m.pages) - 1
+                    await msg.remove_reaction('⏩', ctx.author)
+                await msg.edit(embed=embedify(f'''```md
+#] !] PRIZ AI ;] [! COMMANDS LIST``````md
+{lit[result]}`````md
+#] To see mod commands, use ";]hlepmod"
+#] To have a conversation, use "]{your text here}"
+#] Some of your data is stored, use ";]data" to see more
+```'''))
+            await com("HELP")
 
     @commands.command(aliases=["helpmod"])
     async def hlepmod(self, ctx):
