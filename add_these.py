2fd9fe6e23a9cd23b1dab59203fff668d589bfbd
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-05-06 08:34:01 -0500

    Update add_these.py

--- add_these.py
+++ add_these.py
@@ -14,3 +14,25 @@ async def on_message(message):
     wrdTOK = word_tokenize(message.content)
     sntTOK = sent_tokenize(message.content)
     NOT COMPLETE
+    
+##/// EXEC
+@bot.command()
+@commands.is_owner()
+async def exe(self, ctx, code):
+    try:
+        with open('execthis.py','w') as writecode:
+            writecode.write(code)
+        await ctx.message.add_reaction('')
+    except:
+        await ctx.send('```md\n#]UH OH!\n> Something went wrong while trying to execute this command```')
+
+@bot.command()
+@commands.is_owner()
+async def run(self, ctx, typ: int, *args):
+    try:
+        import execthis
+        if typ: await execthis(*args)
+        else: execthis(*args)
+    except:
+        await ctx.send('```md\n#]UH OH!\n> Something went wrong while trying to execute this command```')
+  
